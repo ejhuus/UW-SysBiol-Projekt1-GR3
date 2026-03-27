af31777c9811a6d15c6c9219e4a917ebf461e548
Mykyta <nikitahrabust@gmail.com>
2026-03-27 20:57:51 +0100

    first iteration of PeriodicConstEnvironment

--- periodic_environment.py
+++ periodic_environment.py
@@ -2,13 +2,34 @@
 
 import numpy as np
 from strategies import EnvironmentDynamics
+from math import sin
 
 class PeriodicConstEnvironment(EnvironmentDynamics):
-    def __init__(self, alpha0, ):
-        pass
+    current_generation = 0
+
+    def __init__(self, zero_crossing: np.ndarray, amplitude: np.ndarray, period:np.ndarray, phase:np.ndarray, delta: float = 0.0):
+        """
+            :param zero_crossing: punk zerowy, czyli wartość wokół której "waha się" optimum.
+            :param delta: odch. std. losowych fluktuacji wokół c (0 = brak szumu)
+            :param amplitude: amplituda funkcji, czyli największe możliwe odchylenie od punktu zerowego
+            :param period: okres sinusoidy w generacjach
+            :param phase: # TODO opisac to
+
+        """
+        # setting parameters
+        self.zero_crossing = np.array(zero_crossing, dtype=float)
+        self.amplitude = amplitude
+        self.period = period
+        self.phase = phase
+        self.delta = float(delta)
+
 
     def update(self) -> None:
-        pass
+        self.current_generation += 1
 
     def get_optimal_phenotype(self):
-        pass
+        n = len(self.zero_crossing)
+        return self.zero_crossing + self.amplitude * sin(np.full(n,self.current_generation) / self.period + self.phase)
+
+    def set_current_generation(self, generation: int):
+        self.current_generation = generation
